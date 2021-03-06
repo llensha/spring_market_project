952e5b25a0f3e28ccacd03c7e8e46e7838197c16
llensha <llensha@mail.ru>
2021-03-06 19:30:57 +0300

    Homework for Lesson 2.4

---
+++ src/main/java/ru/geekbrains/spring/market/endpoints/ProductEndpoint.java
@@ -0,0 +1,47 @@
+package ru.geekbrains.spring.market.endpoints;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import ru.geekbrains.spring.market.services.ProductService;
+import ru.geekbrains.spring.market.soap.GetAllProductsResponse;
+import ru.geekbrains.spring.market.soap.GetProductByIdRequest;
+import ru.geekbrains.spring.market.soap.GetProductByIdResponse;
+
+@Endpoint
+@RequiredArgsConstructor
+public class ProductEndpoint {
+
+    private static final String NAMESPACE_URI = "http://www.market.ru/spring/ws/products";
+    private final ProductService productService;
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getProductByIdRequest")
+    @ResponsePayload
+    public GetProductByIdResponse getProductById(@RequestPayload GetProductByIdRequest request) {
+        GetProductByIdResponse response = new GetProductByIdResponse();
+        response.setProduct(productService.getById(request.getId()));
+        return response;
+    }
+
+    /*
+        Пример запроса: POST http://localhost:8189/ws
+
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ps="http://www.market.ru/spring/ws/products">
+            <soapenv:Header/>
+            <soapenv:Body>
+                <ps:getAllProductsRequest/>
+            </soapenv:Body>
+        </soapenv:Envelope>
+     */
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getAllProductsRequest")
+    @ResponsePayload
+    public GetAllProductsResponse getAllProducts() {
+        GetAllProductsResponse response = new GetAllProductsResponse();
+        productService.getAllProducts().forEach(response.getProducts()::add);
+        return response;
+    }
+
+}
